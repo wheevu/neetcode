2df9ca1778a9b9390f00599d9bb83fa6910e952c
Huy Vũ (Josh) <fixinit286@gmail.com>
2026-04-01 10:15:01 +0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // create map number -> frequency
+        unordered_map<int, int> freq;
+
+        // ## Count the frequencies
+        // loop over every element of vector nums with int num
+        // increase the count for the key in nums matching num; otherwise initialize it at 0
+
+        for (int num : nums){
+            freq[num]++;
+        }
+
+        // ## Make vector storing the {frequency, num} from map
+        // create vector with pairs as elements
+        // loop over every element PAIRS of map freq
+        // add new pair to vector at the end
+        vector<pair<int, int>> arr;
+        
+        for (auto& entry : freq){
+            arr.push_back({entry.second, entry.first});
+        }
+
+
+        // ## Sorting the vector from start to end by descending order
+        sort(arr.begin(), arr.end(), greater<pair<int, int>>());
+
+        // ## Create answer vector - which is the k element with the highest frequencies 
+        // add the number from the pair vector to the answer vector
+        vector<int> result;
+
+        for (int i = 0; i < k; i++){
+            result.push_back(arr[i].second);
+        }
+
+        // ## Print result
+        return result;
+    }
+};
